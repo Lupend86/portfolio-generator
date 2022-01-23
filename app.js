f7163b288c62e7657f5a0c8f4c11736925b5a32a
James Scott <jamesdscottjr@gmail.com>
2022-01-23 18:51:11 -0500

    user input prompt

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+/* const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+const pageHTML = generatePage(name, github);
 
-console.log(profileDataArgs);
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
+fs.writeFile('index.html', pageHTML, err => {
+    if (err) throw err;
 
     console.log('Portfolio complete! Checkout index.html to see the output!');
 });
-
+ */
 
 /* const printProfileData = profileDataArr => {
     // This...
